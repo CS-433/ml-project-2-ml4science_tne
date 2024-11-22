d01d833cb2d9967f804ec96f4a5403dbb5d42da9
samik29sm <sarah@mikami.ch>
2024-11-22 18:07:44 +0100

    functions definition for preprocessing

---
+++ Code/utils.py
@@ -0,0 +1,21 @@
+import pickle
+
+
+def load_pickle(data_path):
+    filename = data_path +  '.pickle'
+    with open(filename, 'rb') as f:
+        dataset = pickle.load(f)
+    return dataset
+
+def get_highest_number_of_channels(df):
+    """
+    Get the highest number of channels throughout participants and sessions
+
+    Args:
+        data (DataFrame): dataset
+
+    Returns:
+        int: highest number of channels throughout participants and sessions
+    """
+    
+    return 0
